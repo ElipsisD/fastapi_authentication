99db380753163bd3e626c0474ed48354a36f3f42
Dmitry <da-boyko@mail.ru>
2024-07-19 14:23:22 +0700

    feat: include note router

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from core.models import db_manager, Base
+from api import router as api_router
 
 
 @asynccontextmanager
@@ -13,13 +14,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-
-@app.get("/")
-def hello_world() -> dict[str, str]:
-    return {
-        "data": "Hello World",
-    }
+app.include_router(router=api_router, prefix="/api")
 
 
 if __name__ == "__main__":
