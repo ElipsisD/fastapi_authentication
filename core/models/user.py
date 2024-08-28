3802fcf6c37dbfbc79d502b8ad433b2cac97aa23
Dmitry <da-boyko@mail.ru>
2024-08-28 20:58:18 +0700

    feat: change username constrain and clear migrations

--- core/models/user.py
+++ core/models/user.py
@@ -1,9 +1,9 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models.base import Base
 
 
 class User(Base):
-    username: Mapped[str]
+    username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
     active: Mapped[bool]
