b9718c05490cb832aa87d0430be7a6014eb35f77
Dmitry <da-boyko@mail.ru>
2024-09-10 21:25:17 +0700

    ref: create mongo db connection manager

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,12 @@ __all__ = (
     "User",
     "db_manager",
     "DatabaseManager",
+    "MongoDBManager",
+    "mongo_db_manager",
 )
 
 from .base import Base
 from .db_manager import DatabaseManager, db_manager
+from .mongo_db_manager import MongoDBManager, mongo_db_manager
 from .note import Note
 from .user import User
