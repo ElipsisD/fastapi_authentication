bab963d09c6175b3f2c5194f0f0bba37f4fcf29f
Dmitry <da-boyko@mail.ru>
2024-09-10 21:19:39 +0700

    ref: change app starting type

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from contextlib import asynccontextmanager
-
-import uvicorn
 from fastapi import FastAPI
-from core.models import db_manager, Base
+
 from api import router as api_router
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(router=api_router, prefix="/api")
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
