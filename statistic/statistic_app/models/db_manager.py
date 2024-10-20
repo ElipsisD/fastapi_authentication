93cd438f20fd1e0c76951dcccac606a204dad9c0
Dmitry <da-boyko@mail.ru>
2024-10-20 18:43:52 +0700

    chore: change mongo service name

--- statistic/statistic_app/models/db_manager.py
+++ statistic/statistic_app/models/db_manager.py
@@ -5,7 +5,7 @@ from statistic_app.config import settings
 
 class DBManager:
     def __init__(self) -> None:
-        url = f"mongodb://{settings.mongo_db.user}:{settings.mongo_db.password}@mongo_db:27017"
+        url = f"mongodb://{settings.mongo_db.user}:{settings.mongo_db.password}@statistic_mongo:27017"
         self._client = AsyncIOMotorClient(url)
         self._db = self._client[settings.mongo_db.db_name]
         self.collection = self._db[settings.mongo_db.collection_name]
